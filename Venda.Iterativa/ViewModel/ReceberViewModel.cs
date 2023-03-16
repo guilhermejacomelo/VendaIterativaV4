3ea248046293d630a0d628b8c72579cb2284aa03
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-03-16 19:36:21 -0300

    Implementações iniciais tela recebimento

---
+++ Venda.Iterativa/ViewModel/ReceberViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venda.Iterativa.Classes;
+using Venda.Iterativa.Model;
+
+namespace Venda.Iterativa.ViewModel
+{
+    internal sealed class ReceberViewModel : AbstractViewModel
+    {
+        public ReceberViewModel(PedidoModel pedido) 
+            : base("UMFG | Receber")
+        {
+
+        }
+    }
+}
